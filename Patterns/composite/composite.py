156423dc66537293368ce6e656889300cbb2997c
Stefan Draghici <stdr@consignor.com>
2019-12-23 11:49:57 +0200

    composite 2

--- Patterns/composite/composite.py
+++ Patterns/composite/composite.py
@@ -1,50 +1,51 @@
-
 class GraphicObject:
 	def __init__(self, color=None):
 		self.color=color
 		self.children=[]
-		self._name='Group'
+		self.__name='group'
 		
 	@property
 	def name(self):
-		return self._name
+		return self.__name
 		
 	@name.setter
 	def name(self, value):
-		self._name=value
+		self.__name=value
 		
-	def _print(self, items, depth):
+	def __print(self, items, depth):
 		items.append('*'*depth)
 		if self.color:
 			items.append(self.color)
 		items.append(f'{self.name}\n')
 		for child in self.children:
-			child._print(items, depth+1)
+			child.__print(items, depth+1)
 			
 	def __str__(self):
 		items=[]
-		self._print(items, 0)
+		self.__print(items, 0)
 		return ''.join(items)
 		
 		
 class Circle(GraphicObject):
-	@property
+	@propoerty
 	def name(self):
 		return 'Circle'
-
-
+		
+		
 class Square(GraphicObject):
-	@property
+	@propoerty
 	def name(self):
 		return 'Square'
 		
-
+		
 #test
 drawing=GraphicObject()
 drawing.name='Drawing'
-drawing.children.append(Square('red'))
-drawing.children.append(Square('blue'))
-drawing.children.append(Circle('yellow'))
-drawing.children.append(Circle('green'))
+drawing.children.append(Square('Red'))
+drawing.children.append(Circle('Yellow'))
 
-print(drawing)
+group=GraphicObject()
+group.children.append(Circle('Blue'))
+group.children.append(Square('Green'))
+
+drawing.children.append(group)
