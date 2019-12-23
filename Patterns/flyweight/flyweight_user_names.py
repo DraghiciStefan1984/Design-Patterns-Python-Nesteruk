9e76b8dba902c8d1a99c5c5206cd27de948dfbdc
Stefan Draghici <stdr@consignor.com>
2019-12-23 17:15:49 +0200

    flyweight

--- Patterns/flyweight/flyweight_user_names.py
+++ Patterns/flyweight/flyweight_user_names.py
@@ -1,37 +1,52 @@
-import string
 import random
+import string
+import sys
 
 
 class User:
-	def __init__(self, name):
-		self.name=name
-		
-		
+    def __init__(self, name):
+        self.name = name
+
+
 class User2:
-	strings=[]
-	def __init__(self, full_name):
-		def get_or_add(x):
-			if s in self.strings:
-				return self.string.index(s)
-			else:
-				self.strings.append(s)
-				return len(self.strings)-1 
-		self.names=[get_or_add(x) for x in full_name.split(' ')]
-		
-	def __str__(self):
-		return ''.join([self.strings[x] for x in self.names])
-		
+    strings = []
+
+    def __init__(self, full_name):
+        def get_or_add(s):
+            if s in self.strings:
+                return self.strings.index(s)
+            else:
+                self.strings.append(s)
+                return len(self.strings)-1
+        self.names = [get_or_add(x) for x in full_name.split(' ')]
+
+    def __str__(self):
+        return ' '.join([self.strings[x] for x in self.names])
+
 
 def random_string():
-	chars=string.ascii_lowercase
-	return ''.join([random.choice(chars) for x in range(8)])
+    chars = string.ascii_lowercase
+    return ''.join([random.choice(chars) for x in range(8)])
+
 
 #test
-users=[]
-first_names=[random_string() for x in range(100)]
-last_names=[random_string() for x in range(100)]
+users = []
+
+first_names = [random_string() for x in range(100)]
+last_names = [random_string() for x in range(100)]
 
 for first in first_names:
 	for last in last_names:
-		#users.append(User(f'{first} {last}'))
-		users.append(User2(f'{first} {last}'))
+		users.append(User(f'{first} {last}'))
+
+u2 = User2('Jim Jones')
+u3 = User2('Frank Jones')
+print(u2.names)
+print(u3.names)
+print(User2.strings)
+
+users2 = []
+
+for first in first_names:
+	for last in last_names:
+		users2.append(User2(f'{first} {last}'))
