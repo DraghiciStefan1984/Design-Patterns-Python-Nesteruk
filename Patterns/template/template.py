e2a58833739bda5bdda7d9e80c90c6127c4bd55e
Stefan Draghici <stdr@consignor.com>
2020-01-21 11:10:21 +0200

    done 2

--- Patterns/template/template.py
+++ Patterns/template/template.py
@@ -1,51 +1,56 @@
 from abc import ABC
 
-
 class Game(ABC):
-    def __init__(self, number_of_players):
-        self.number_of_players = number_of_players
-        self.current_player = 0
-
-    def run(self):
-        self.start()
-        while not self.have_winner:
-            self.take_turn()
-        print(f'Player {self.winning_player} wins!')
-
-    def start(self): pass
-
-    @property
-    def have_winner(self): pass
-
-    def take_turn(self): pass
-
-    @property
-    def winning_player(self): pass
-
-
+	def __init__(self, num_of_players):
+		self.num_of_players=num_of_players
+		self.current_player=0
+		
+	def run(self):
+		self.start()
+		while not self.have_winner:
+			self.take_turn()
+		print(f'player {self.winner} wins!')
+		
+	def start(self): pass
+	
+	def take_turn(self): pass
+	
+	@property
+	def have_winner(self): pass
+	
+	@property
+	def winner(self): pass
+	
+	
 class Chess(Game):
-    def __init__(self):
-        super().__init__(2)
-        self.max_turns = 10
-        self.turn = 1
-
-    def start(self):
-        print(f'Starting a game of chess with {self.number_of_players} players.')
-
-    @property
-    def have_winner(self):
-        return self.turn == self.max_turns
-
-    def take_turn(self):
-        print(f'Turn {self.turn} taken by player {self.current_player}')
-        self.turn += 1
-        self.current_player = 1 - self.current_player
-
-    @property
-    def winning_player(self):
-        return self.current_player
-
-
+	def __init__(self):
+		super().__init__(2)
+		self.max_turns=10
+		self.turn=1
+		
+	def run(self):
+		super().run()
+		
+	def start(self):
+		print('starting a chess game.')
+		
+	def take_turn(self):
+		print(f'turn {self.turn} taken by player {self.current_player}')
+		self.turn+=1
+		self.current_player=1-self.current_player
+		
+	
+	@property
+	def have_winner(self):
+		return self.turn==self.max_turns
+	
+	@property
+	def winner(self):
+		return self.current_player
+		
+		
 #test
-chess = Chess()
+chess=Chess()
 chess.run()
+
+
